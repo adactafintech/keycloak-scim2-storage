a758138f5adab76675b9e186eb1a98a329da3c5c
Matej Hvezda <matej.hvezda@adacta-fintech.com>
2024-09-26 08:36:43 +0200

    group sync commented out

--- src/main/java/dev/suvera/keycloak/scim2/storage/storage/JobEnqueuer.java
+++ src/main/java/dev/suvera/keycloak/scim2/storage/storage/JobEnqueuer.java
@@ -81,23 +81,54 @@ public class JobEnqueuer {
     }
 
     public void enqueueGroupCreateJob(String realmId, String groupId) {
-        // not supported
+        // ScimSyncJobQueue entity = createJobQueue(realmId);
+        // entity.setAction(ScimSyncJob.CREATE_GROUP);
+        // entity.setGroupId(groupId);
+
+        // run(entity);
+
+        // log.infof("Group with id %s scheduled to be added.", groupId);
     }
 
     public void enqueueGroupUpdateJob(String realmId, String groupId) {
-        // not supported
+        // ScimSyncJobQueue entity = createJobQueue(realmId);
+        // entity.setAction(ScimSyncJob.UPDATE_GROUP);
+        // entity.setGroupId(groupId);
+
+        // run(entity);
+
+        // log.infof("Group with id %s scheduled to be added.", groupId);
     }
 
     public void enqueueGroupDeleteJob(String realmId, String groupId) {
-        // not supported
+        // ScimSyncJobQueue entity = createJobQueue(realmId);
+        // entity.setAction(ScimSyncJob.DELETE_GROUP);
+        // entity.setGroupId(groupId);
+
+        // run(entity);
+
+        // log.infof("Group with id %s scheduled to be deleted.", groupId);
     }
 
     public void enqueueGroupJoinJob(String realmId, String groupId, String userId) {
-        // not supported
+        // ScimSyncJobQueue entity = createJobQueue(realmId);
+        // entity.setAction(ScimSyncJob.JOIN_GROUP);
+        // entity.setGroupId(groupId);
+        // entity.setUserId(userId);
+        // run(entity);
+
+        // log.infof("User with id %s scheduled to join group with id %s.", userId, groupId);
     }
 
     public void enqueueGroupLeaveJob(String realmId, String groupId, String userId) {
-        // not supported
+        // ScimSyncJobQueue entity = createJobQueue(realmId);
+        // entity.setAction(ScimSyncJob.LEAVE_GROUP);
+        // entity.setGroupId(groupId);
+        // entity.setUserId(userId);
+
+        // run(entity);
+
+        // log.infof("User with id %s scheduled to leave group with id %s.", userId, groupId);
     }
 
     private ScimSyncJobQueue createJobQueue(String realmId) {
