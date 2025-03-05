40cbef35310312603cf2a368cf9fc222ad8cc051
Jernej <jernej.aubersek@adacta-fintech.com>
2025-03-05 08:55:38 +0100

    changed caching of client key is now hash of component model properties

--- src/main/java/dev/suvera/keycloak/scim2/storage/storage/ScimClient2Factory.java
+++ src/main/java/dev/suvera/keycloak/scim2/storage/storage/ScimClient2Factory.java
@@ -4,6 +4,7 @@ import dev.suvera.scim2.schema.ex.ScimException;
 import org.keycloak.component.ComponentModel;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -14,14 +15,21 @@ public class ScimClient2Factory {
     public static final Map<String, ScimClient2> instances = new ConcurrentHashMap<>();
 
     public static synchronized ScimClient2 getClient(ComponentModel componentModel) throws ScimException {
-        if (!instances.containsKey(componentModel.getId())) {
-            ScimClient2 scimClient = new ScimClient2(componentModel);
+        String id = componentModel.getId();
+        ScimClient2 scimClient = instances.get(id);
+
+        if (scimClient != null && scimClient.clientHashCode != ScimClient2.getClientHashFromModel(componentModel)) {
+            scimClient = null;
+            instances.remove(id);
+        }
+
+        if (scimClient == null) {
+            scimClient = new ScimClient2(componentModel);
 
             scimClient.validate();
 
             instances.put(componentModel.getId(), scimClient);
         }
-
-        return instances.get(componentModel.getId());
+        return scimClient;
     }
 }
